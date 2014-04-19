f3acaf8a7dc709342042cdef8798c46be83bf18e
Jason Ray <jason.ray@agilex.com>
2014-04-18 21:06:35 -0400

    simplified example

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 var connect = require("connect");
 var http = require("http");
 
+var numberOfActiveCalls = 0;
 var app = connect()
-  .use(connect.logger('dev'))
-  .use(connect.static('public'))
-  .use(function(req, res){
-    res.end('hello world\n');
-  });
+	.use(connect.logger('dev'))
 
-http.createServer(app).listen(3000);
+.use(function(req, res, next) {
+	console.log('active calls: ' + (++numberOfActiveCalls));
+
+	function onEndCall() {
+		res.removeListener('finish', onEndCall);
+		res.removeListener('close', onEndCall);
+		console.log('active calls: ' + (--numberOfActiveCalls));
+	}
+
+	res.on('finish', onEndCall);
+	res.on('close', onEndCall);
+	next();
+})
+
+.use(function(req, res, next) {
+	// console.log('begin PEP');
+	setTimeout(function() {
+		// console.log('end PEP');
+		next();
+	}, 500);
+})
+
+.use(function(req, res, next) {
+	// console.log('begin resource processing, fetch data which will take 1s');
+	setTimeout(function() {
+		// console.log('end fetch data');
+		res.end('hello world\n');
+	}, 1000);
+});
+
+http.createServer(app).listen(8888);
