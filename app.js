3a4c54b7cd0857739fff1d56002f04737947db80
Jason Ray <jason.ray@agilex.com>
2014-04-20 08:23:12 -0400

    cleanup comments

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var connect = require("connect");
-var http = require("http");
 var logger = require('bunyan').createLogger({
 	name: "server"
 });
@@ -7,15 +6,21 @@ var port = 8888;
 
 var numberOfActiveCalls = 0;
 
+// I created this as a sample custom module
+// counts the number of active calls and 
+// the total number of calls
 var activeCallFilterModule = require('./lib/request-filters/active-call-counter');
 
 var app = connect()
 
 .use(connect.logger('dev'))
 
+// link the custom module to certain endpoints
 .use('/api', activeCallFilterModule.activeCallFilter())
 .use('/public', activeCallFilterModule.activeCallFilter())
 
+// demonstrating that I can then have another filter
+// to re-use a module for different purpose
 .use(function(req, res, next) {
 	logger.info('check for number of calls?');
 	logger.info('active: ' + activeCallFilterModule.getNumberOfActiveCalls());
@@ -23,6 +28,9 @@ var app = connect()
 	next();
 })
 
+// demonstrate that we could create a policy enforcement point (PEP)
+// which would likely call out to a policy decision point (PDP)
+// assume that this would have latency of 500ms
 .use('/api', function(req, res, next) {
 	logger.info('checking PEP');
 	setTimeout(function() {
@@ -31,6 +39,7 @@ var app = connect()
 	}, 500);
 })
 
+// this would represent an expensive resource with 2000ms latency
 .use('/api/fetch/expensive', function(req, res, next) {
 	// logger.info('begin resource processing, fetch data which will take 1s');
 	setTimeout(function() {
@@ -39,6 +48,7 @@ var app = connect()
 	}, 2000);
 })
 
+// this would represent an expensive resource with 500ms latency
 .use('/api/fetch/light', function(req, res, next) {
 	// logger.info('begin resource processing, fetch data which will take 1s');
 	setTimeout(function() {
@@ -47,12 +57,16 @@ var app = connect()
 	}, 500);
 })
 
+// this would represent a very light resource with no latency
 .use('/public/hello', function(req, res, next) {
 	res.end('hello world\n');
 })
 
+// this would represent an admin type of resource to display
+// info like the number of calls currently being performed
 .use('/admin/stats', activeCallFilterModule.activeCallResource());
 
-http.createServer(app).listen(port, function() {
+// this starts the actual http server
+require("http").createServer(app).listen(port, function() {
 	logger.info("now listening on %s", port);
 });
